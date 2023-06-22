31ad3d833b625da66d5a4dd221ac4b0db1e3be17
Alex-Shish <alexshishg@gmail.com>
2023-06-22 21:30:50 +0300

    add solution of "Count IP Address" 5kyu

--- Count-ip-addresses.py
+++ Count-ip-addresses.py
@@ -9,3 +9,17 @@ Examples
 * With input "10.0.0.0", "10.0.1.0"   => return  256
 * With input "20.0.0.10", "20.0.1.0"  => return  246"""
 
+def ips_between(start, end):
+    sm = 0
+    st = start.split(".")
+    stn = [int(x) for x in st]
+    fn = end.split(".")
+    fnn = [int(x) for x in fn]
+    i = 3
+    while i >= 0:
+        sm += 256 ** (3 - i) * (fnn[i] - stn[i])
+        i -= 1
+    return sm
+print(ips_between("10.0.0.0", "10.0.0.50"))
+print(ips_between("10.0.0.0", "10.0.1.0"))
+print(ips_between("20.0.0.10", "20.0.1.0"))
